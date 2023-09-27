1bed1c45c7e32ee9a170f204e489ff0f71aebb6a
Harrison Loh <hankman.loh@gmail.com>
2023-09-27 00:48:42 -0400

    Create python_tool_script_template.py

---
+++ contents/00-config/python_tool_script_template.py
@@ -0,0 +1,36 @@
+"""
+Purpose of code:
+Input directory:
+Inputs data:
+Output directory:
+Output data:
+"""
+#%% Imports
+# Default python imports
+
+
+# Installed Module imports
+
+
+# Local function imports
+
+
+#%% Definitions
+# Class definitions
+
+
+# Function definitions
+
+
+#%% Main
+# Data import
+
+
+# Preprocessing
+
+
+# Calculations
+
+
+# Results saving
+

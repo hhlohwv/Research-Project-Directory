e5d6dfa9955a6dde26fe3c9560cea9b9332a8a1d
Harrison Loh <hankman.loh@gmail.com>
2023-10-05 12:08:09 -0400

    Delete create_data_folder.bat

--- contents/04-data/create_data_folder.bat
+++
@@ -1,4 +0,0 @@
-:: Create folder for storing a set of data according the user's input
-:: Also create an info file in the folder to note what notebook folder to refer to for the experiment used to create the data
-
-pause
